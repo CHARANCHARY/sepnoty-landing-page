a2d1f97a272710a281fe923338f5f45102e9938c
CHARANCHARY <charanchary999@gmail.com>
2024-02-21 18:58:00 +0530

    .

--- src/components/Footer/ContactSection/styled.js
+++ src/components/Footer/ContactSection/styled.js
@@ -13,13 +13,20 @@ export const ContactType = styled.p`
   @media screen and (min-width: 768px) {
     font-size: 12px;
   }
+  
 `;
 
 /* @media screen and (max-width: 576px) {
   .contact-type {
     font-size: 8px;
   }
-
+ @media screen and (max-width:455px){
+    width: 800px;
+    border-radius:10px;
+    margin-left:-70px;
+    padding: 20px 0px 20px 15px;
+    margin-right: -90px;
+  }
   .contact-items {
     padding-top: 7px;
   }
