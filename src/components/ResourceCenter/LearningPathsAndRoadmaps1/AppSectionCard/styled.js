ff9e9223b930078bd858a9ad7d1584b0f788b12e
CHARANCHARY <charanchary999@gmail.com>
2024-02-28 17:02:37 +0530

    .

---
+++ src/components/ResourceCenter/LearningPathsAndRoadmaps1/AppSectionCard/styled.js
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+export const AppCardCon=styled.div`
+display:flex;
+flex-direction:column;
+padding:10px;
+width:75%;
+margin-bottom:30px;
+`;
+export const WebSectionTwoHeading=styled.h2`
+color:rgba(43, 69, 155, 1);
+font-size:24px;
+margin:0px 0px 20px 30px;
+`;
+export const HeadingSpan=styled.span`
+width:65px;
+height:8%;
+margin-top: -6px;
+margin-left:15px;
+position:absolute;
+background-color:rgba(129, 150, 219, 0.25);
+z-index:0;
+`;
+export const AppAbout=styled.p`
+font-size:16px;
+width:65%;
+color:white;
+margin-left:40px;
+margin-top:15px;
+margin-bottom:15px;
+`;
+export const ImgCon = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 30%;
+  position: relative; /* Add this line */
+  justify-content: flex-end; /* Add this line */
+  margin-left:40px;
+`;
+export const InsertImg=styled.img`
+
+`;
+export const WebTechButton = styled.button`
+  margin-bottom: 0px;
+  padding-bottom: 0px;
+  cursor: pointer;
+  gap: 0;
+  padding: 8px;
+  border-radius: 7px;
+  background-color: rgba(217, 217, 217, 1);
+  color: rgba(43, 69, 155, 1);
+  margin-top: -20px; 
+   
+`;
---
+++ src/components/ResourceCenter/LearningPathsAndRoadmaps1/MobDevPage.js
@@ -0,0 +1,61 @@
+import FooterSection from "../../Footer/FooterSection";
+import InqContact from "../../InquiryContact/InquiryContactSection";
+import AppSectionCard from "./AppSectionCard/AppSectionCard";
+import MobSectionOne from "./MobSectionOne/MobSectionOne";
+import appimg1 from './images/appimg1.svg';
+import appimg2 from './images/appimg2.svg';
+import appimg3 from './images/appimg3.svg';
+import appimg4 from './images/appimg4.svg';
+import {
+    MobSection,
+    WebPageTitle,
+    WebSpan,
+    WebHeading,
+    CapabilityHeading,
+    AppDetailsContainer,
+} from './styled';
+const AppDetails=[
+    {
+        id:1,
+        title:'Android App Development',
+        about:'Our mobile application development company in India has key competence in Android, having developed all our applications in it.',
+        img:appimg1,
+    },
+    {
+        id:2,
+        title:'IOS App Development',
+        about:'iOS app development refers to the process of creating applications specifically for Apples iOS platform, which runs on devices such as the iPhone, iPad, and iPod Touch. Here are the key aspects and steps involved in iOS app development',
+        img:appimg2,
+    },
+    {
+        id:3,
+        title:'Flutter App Development',
+        about:'Flutter is an open-source UI software development kit (SDK) developed by Google for building natively compiled applications for mobile, web, and desktop from a single codebase.',
+        img:appimg3,
+    },
+    {
+        id:4,
+        title:'React App Development',
+        about:'React is an open-source JavaScript library developed by Facebook for building user interfaces (UIs) for web and mobile applications. It is widely used for creating dynamic and interactive UI components.',
+        img:appimg4,
+    },
+];
+const LearningRoadsMaps = () => {
+  return (
+    <MobSection>
+    <WebPageTitle>Services &gt; <WebSpan>Mobile App development</WebSpan></WebPageTitle>
+    <WebHeading>Services</WebHeading>
+    <MobSectionOne />
+    <CapabilityHeading>App Development Capabilities</CapabilityHeading>
+    <AppDetailsContainer>
+    {AppDetails.map((eachItem) => (
+        <AppSectionCard key={eachItem.id} details={eachItem} />
+      ))}
+    </AppDetailsContainer>
+    <InqContact />
+    <FooterSection />
+    </MobSection>
+  )
+}
+
+export default LearningRoadsMaps;
