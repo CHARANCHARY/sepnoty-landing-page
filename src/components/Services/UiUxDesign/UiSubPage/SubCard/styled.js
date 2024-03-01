ae81a3c811be5222acaad48b6935ab3026b1d022
padmalatchi <padmalatchi@gmail.com>
2024-03-01 07:59:03 +0530

    subpagenavigation added

---
+++ src/components/Services/UiUxDesign/UiSubPage/SubCard/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const SubCardSection=styled.div`
+
+background-color:green;
+display:flex;
+flex-direction:column;
+justify-content:center;
+height:50vh;
+padding:20px;
+`;
